976ae175a18ab87abc3a5afc37ba020a48d0510f
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-19 14:23:30 +0900

    PDF選択ボタンを追加

--- Bookfy/PDFVIew.swift
+++ Bookfy/PDFVIew.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct PDFVIew: View {
+    @State private var selectedPDF: URL?
+    @State private var showDocumentPicker = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List{
+                // PDF選択ボタン
+                Section {
+                    Button("PDFを選択") {
+                        showDocumentPicker.toggle()
+                    }
+                    .sheet(isPresented: $showDocumentPicker) {
+                        DocumentPicker(selectedURL: $selectedPDF)
+                    }
+                    
+                    if let pdfURL = selectedPDF {
+                        NavigationLink("選択したPDFを表示") {
+                            //PDFViewer(fileURL: pdfURL)
+                        }
+                    }
+                } header: {
+                    Text("Select PDF from Files")
+                        .textCase(.none)
+                }
+            }
+        }
     }
 }
 
