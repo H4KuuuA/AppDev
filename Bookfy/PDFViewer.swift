af5bdf2ed7811dce1474c9eb686ff5990e1d16b9
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-19 14:25:06 +0900

    PDFViewrファイルを作成

---
+++ Bookfy/PDFViewer.swift
@@ -0,0 +1,18 @@
+//
+//  PDFViewer.swift
+//  Bookfy
+//
+//  Created by 大江悠都 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct PDFViewer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PDFViewer()
+}
