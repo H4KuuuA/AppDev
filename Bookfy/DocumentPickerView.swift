f1746ecbdb827b9404b471671acad4ba3630f22c
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-19 12:59:02 +0900

    DocumentPickerViewファイルを作成

---
+++ Bookfy/DocumentPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentPickerView.swift
+//  Bookfy
+//
+//  Created by 大江悠都 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct DocumentPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentPickerView()
+}
