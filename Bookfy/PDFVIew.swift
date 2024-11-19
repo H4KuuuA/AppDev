123c930d5bc69ea97e9b6c29527eadb82bd86e3a
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-19 14:01:39 +0900

    PDFViewファイルを作成

---
+++ Bookfy/PDFVIew.swift
@@ -0,0 +1,18 @@
+//
+//  PDFVIew.swift
+//  Bookfy
+//
+//  Created by 大江悠都 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct PDFVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PDFVIew()
+}
