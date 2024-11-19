600b9e1e7dd833dd73ec3bb62b20f9e0595d1b73
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-19 13:51:15 +0900

    test

--- Bookfy/ContentView.swift
+++ Bookfy/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("git baka")
+            Text("git kuso")
         }
         .padding()
     }
