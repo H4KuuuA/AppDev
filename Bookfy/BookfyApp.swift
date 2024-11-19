9b8e4a3555905ef7f1c5b56cf8db63dee3d4b3bd
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-19 11:49:39 +0900

    Initial Commit

---
+++ Bookfy/BookfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookfyApp.swift
+//  Bookfy
+//
+//  Created by 大江悠都 on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct BookfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookfy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookfy
+//
+//  Created by 大江悠都 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
