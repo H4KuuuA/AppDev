805f313afc925f1b442a9bcebaf0c57aff761460
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-20 09:35:40 +0900

    Scandocumentファイルを作成

---
+++ Bookfy/Scandocument.swift
@@ -0,0 +1,18 @@
+//
+//  Scandocument.swift
+//  Bookfy
+//
+//  Created by 大江悠都 on 2024/11/20.
+//
+
+import SwiftUI
+
+struct Scandocument: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Scandocument()
+}
