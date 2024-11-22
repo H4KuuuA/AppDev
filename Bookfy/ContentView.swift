41cd5364242a85c65cb2e2a5cdb788c8413539bb
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2024-11-22 14:21:29 +0900

    HomeViewにGeometoryReaderを追加して、必要な引数の追加

--- Bookfy/ContentView.swift
+++ Bookfy/ContentView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var items: [CoverFlowItem] = [.red, .blue, .green, .yellow, .primary].compactMap {
+        return .init(color: $0)
+    }
     var body: some View {
         NavigationStack{
             VStack{
-                
+                HomeView(
+                    itemWidth: 280,
+                    spacing: 0,
+                    rotation: 0,
+                    items: items
+                ) {item in
+                RoundedRectangle(cornerRadius: 20)
+                        .fill(item.color.gradient)
+                }
+                .frame(height: 180)
             }
             .navigationTitle("Bookfy")
         }
--- Bookfy/HomeView.swift
+++ Bookfy/HomeView.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct HomeView<Content: View, Item: RandomAccessCollection>: View where Item.Element: Identifiable{
     var itemWidth : CGFloat
+    var spacing : CGFloat = 0
+    var rotation: Double
     var items: Item
     var content: (Item.Element) -> Content
     
     var body: some View {
-        ScrollView(.horizontal){
-            LazyHStack(spacing: 0){
-                ForEach(items) { item in
-                    content(item)
-                        .frame(width: itemWidth)
+        GeometryReader {
+            let size = $0.size
+            ScrollView(.horizontal){
+                LazyHStack(spacing: 0){
+                    ForEach(items) { item in
+                        content(item)
+                            .frame(width: itemWidth)
+                    }
                 }
+                .padding(.horizontal,(size.width - itemWidth) / 2)
+                .scrollTargetLayout()
             }
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
         }
+        
     }
 }
 
